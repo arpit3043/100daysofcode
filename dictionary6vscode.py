4adb3130f0dc5462f3edda92620b6c234fde64a9
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2020-05-28 19:07:21 +0530

    Add files via upload

---
+++ dictionary6vscode.py
@@ -0,0 +1,11 @@
+N = int(input(n))
+def factorial(n):
+	if N == 0:
+		return 1
+
+	else:
+		N = 1
+		for i in range(1, n+1):
+			return N*factorial(N-1)
+
+			return(N)
---
+++ edx.py
@@ -0,0 +1,12 @@
+n = 6
+
+def factorial(n):
+	if N == 0:
+		return 1
+
+	else:
+		N = 1
+		for i in range(1, n+1):
+			return N*factorial(N-1)
+
+			return(N)
